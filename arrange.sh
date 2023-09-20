fb26edaba2dc0340675432be6e516a70d66326fe
Minho_BBU_01 <58423056+hoeo159@users.noreply.github.com>
2023-09-20 19:31:00 +0900

    Add files via upload

---
+++ arrange.sh
@@ -0,0 +1,112 @@
+#!/bin/bash
+
+cd /root/SWE_2021_41_2023_2_week_4/files
+
+for filename in *
+do
+    if [[ "$filename" == "a"* || "$filename" == "A"* ]]
+    then
+        mv "$filename" "/root/SWE_2021_41_2023_2_week_4/a/"
+
+    elif [[ "$filename" == "b"* || "$filename" == "B"* ]]
+    then
+        mv "$filename" "/root/SWE_2021_41_2023_2_week_4/b/"
+
+    elif [[ "$filename" == "c"* || "$filename" == "C"* ]]
+    then
+        mv "$filename" "/root/SWE_2021_41_2023_2_week_4/c/"
+
+    elif [[ "$filename" == "d"* || "$filename" == "D"* ]]
+    then
+        mv "$filename" "/root/SWE_2021_41_2023_2_week_4/d/"
+
+    elif [[ "$filename" == "e"* || "$filename" == "E"* ]]
+    then
+        mv "$filename" "/root/SWE_2021_41_2023_2_week_4/e/"
+
+    elif [[ "$filename" == "f"* || "$filename" == "F"* ]]
+    then
+        mv "$filename" "/root/SWE_2021_41_2023_2_week_4/f/"
+
+    elif [[ "$filename" == "g"* || "$filename" == "G"* ]]
+    then
+        mv "$filename" "/root/SWE_2021_41_2023_2_week_4/g/"
+
+    elif [[ "$filename" == "h"* || "$filename" == "H"* ]]
+    then
+        mv "$filename" "/root/SWE_2021_41_2023_2_week_4/h/"
+
+    elif [[ "$filename" == "i"* || "$filename" == "I"* ]]
+    then
+        mv "$filename" "/root/SWE_2021_41_2023_2_week_4/i/"
+
+    elif [[ "$filename" == "j"* || "$filename" == "J"* ]]
+    then
+        mv "$filename" "/root/SWE_2021_41_2023_2_week_4/j/"
+
+    elif [[ "$filename" == "k"* || "$filename" == "K"* ]]
+    then
+        mv "$filename" "/root/SWE_2021_41_2023_2_week_4/k/"
+
+    elif [[ "$filename" == "l"* || "$filename" == "L"* ]]
+    then
+        mv "$filename" "/root/SWE_2021_41_2023_2_week_4/l/"
+
+    elif [[ "$filename" == "m"* || "$filename" == "M"* ]]
+    then
+        mv "$filename" "/root/SWE_2021_41_2023_2_week_4/m/"
+
+    elif [[ "$filename" == "n"* || "$filename" == "N"* ]]
+    then
+        mv "$filename" "/root/SWE_2021_41_2023_2_week_4/n/"
+
+    elif [[ "$filename" == "o"* || "$filename" == "O"* ]]
+    then
+        mv "$filename" "/root/SWE_2021_41_2023_2_week_4/o/"
+
+    elif [[ "$filename" == "p"* || "$filename" == "P"* ]]
+    then
+        mv "$filename" "/root/SWE_2021_41_2023_2_week_4/p/"
+
+    elif [[ "$filename" == "q"* || "$filename" == "Q"* ]]
+    then
+        mv "$filename" "/root/SWE_2021_41_2023_2_week_4/q/"
+
+    elif [[ "$filename" == "r"* || "$filename" == "R"* ]]
+    then
+        mv "$filename" "/root/SWE_2021_41_2023_2_week_4/r/"
+
+    elif [[ "$filename" == "s"* || "$filename" == "S"* ]]
+    then
+        mv "$filename" "/root/SWE_2021_41_2023_2_week_4/s/"
+
+    elif [[ "$filename" == "t"* || "$filename" == "T"* ]]
+    then
+        mv "$filename" "/root/SWE_2021_41_2023_2_week_4/t/"
+
+    elif [[ "$filename" == "u"* || "$filename" == "U"* ]]
+    then
+        mv "$filename" "/root/SWE_2021_41_2023_2_week_4/u/"
+
+
+    elif [[ "$filename" == "v"* || "$filename" == "V"* ]]
+    then
+        mv "$filename" "/root/SWE_2021_41_2023_2_week_4/v/"
+
+    elif [[ "$filename" == "w"* || "$filename" == "W"* ]]
+    then
+        mv "$filename" "/root/SWE_2021_41_2023_2_week_4/w/"
+
+    elif [[ "$filename" == "x"* || "$filename" == "X"* ]]
+    then
+        mv "$filename" "/root/SWE_2021_41_2023_2_week_4/x/"
+
+    elif [[ "$filename" == "y"* || "$filename" == "Y"* ]]
+    then
+        mv "$filename" "/root/SWE_2021_41_2023_2_week_4/y/"
+
+    elif [[ "$filename" == "z"* || "$filename" == "Z"* ]]
+    then
+        mv "$filename" "/root/SWE_2021_41_2023_2_week_4/z/"
+    fi
+done
